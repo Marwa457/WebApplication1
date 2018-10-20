fbbfd9cb849e8eea1c2b58bf63b704579f5283d6
mhiri marwa <mhiri.marwa18@gmail.com>
2018-10-20 23:48:58 +0100

    Ajoutez des fichiers projet.

---
+++ WebApplication1/Controllers/PersonnesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonnesController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+        [Authorize]
+
+        // GET: Personnes
+        public ActionResult Index()
+        {
+            return View(db.Personne.ToList());
+        }
+        // GET: Personnes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personne personne = db.Personne.Find(id);
+            if (personne == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personne);
+        }
+
+        // GET: Personnes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Personnes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Personne1,Nom,Prenom")] Personne personne)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Personne.Add(personne);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(personne);
+        }
+
+        // GET: Personnes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personne personne = db.Personne.Find(id);
+            if (personne == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personne);
+        }
+
+        // POST: Personnes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Personne1,Nom,Prenom")] Personne personne)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(personne).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(personne);
+        }
+
+        // GET: Personnes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personne personne = db.Personne.Find(id);
+            if (personne == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personne);
+        }
+
+        // POST: Personnes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Personne personne = db.Personne.Find(id);
+            db.Personne.Remove(personne);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
